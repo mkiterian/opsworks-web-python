2f686ba2207d260c8639edc08b7e809b3b125301
mkiterian <hmukite@gmail.com>
2018-09-20 12:45:26 -0400

    change version number

--- opsworks_deploy_python/metadata.rb
+++ opsworks_deploy_python/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "alecpm@gmail.com"
 license          "BSD License"
 description      "Deploys and configures zc.buildout based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports "ubuntu"
 
